379c6a2a516d36a2e79ceca62b02c331674b73a6
Usernameimalicolak <19acolak99@gmail.com>
2023-07-23 14:09:50 -0500

    Videos

--- src/pages/videos.jsx
+++ src/pages/videos.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const videos = () => {
-  return <div>videos</div>;
+  return (
+    <div className="p-24 justify-center items-center flex">
+      {" "}
+      Under construction... coming soon!
+    </div>
+  );
 };
 
 export default videos;
