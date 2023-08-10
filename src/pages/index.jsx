2eb3d40663d54e9bd2085dc31b1e6297b1061755
Usernameimalicolak <19acolak99@gmail.com>
2023-08-09 21:42:03 -0500

    Text edit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,8 +35,8 @@ export default function Home() {
                 className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
-                Front-end, Backend, Fullstack. I am dedicated to turning ideas
-                into web applications that solve a problem.
+                Software Engineer, Product Manager, Data Scientist. I am
+                dedicated to turning solving problems with code.
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
